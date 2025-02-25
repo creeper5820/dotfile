465f0f4f8d2647420f7b07e246cc7aca6774e947
creeper5820 <cqq1960180796@163.com>
2025-02-26 07:37:22 +0800

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -18,7 +18,8 @@ vim.api.nvim_set_keymap("i", "<C-BS>", "<C-w>", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("i", "<C-Del>", "<C-o>dw", { noremap = true, silent = true })
 
 -- exit terminal mode
-vim.api.nvim_set_keymap("t", "<C-[>", "<C-\\><C-n>", { noremap = true })
+vim.api.nvim_set_keymap("t", "jj", "<C-\\><C-n>", { noremap = true })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "退出插入模式 (jk)" })
 
 -- open zsh in new terminal tab
 vim.api.nvim_set_keymap("n", "<Leader>tz", ":terminal zsh<CR>", { noremap = true, silent = true })
