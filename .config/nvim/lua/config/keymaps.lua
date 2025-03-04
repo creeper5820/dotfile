bbe39a6018ab93177b043bbd718dd1402cde2395
creeper5820 <cqq1960180796@163.com>
2025-03-04 23:31:04 +0800

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,16 +8,21 @@ vim.api.nvim_set_keymap("i", "<A-L>", "<C-o>w", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("i", "<A-H>", "<C-o>b", { noremap = true, silent = true })
 
 -- page
-vim.api.nvim_set_keymap("n", "<CS-j>", "<C-d>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<CS-k>", "<C-u>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-d>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-u>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<CS-j>", "<C-f>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<CS-k>", "<C-b>", { noremap = true, silent = true })
 
 -- delete
 vim.api.nvim_set_keymap("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<C-Del>", "<C-o>dw", { noremap = true, silent = true })
 
--- exit terminal mode
+-- terminal mode
 vim.api.nvim_set_keymap("t", "jk", "<C-\\><C-n>", { noremap = true })
 vim.keymap.set("i", "jk", "<Esc>", { desc = "退出插入模式 (jk)" })
+
+vim.api.nvim_set_keymap("t", "<A-j>", "<C-\\><C-n><C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<A-k>", "<C-\\><C-n><C-w>k", { noremap = true, silent = true })
 
 -- open zsh in new terminal tab
 vim.api.nvim_set_keymap("n", "<Leader>tz", ":terminal zsh<CR>", { noremap = true, silent = true })
