43109a0635ca4ec8074ddb3e32e5a50a716bec8d
creeper5820 <cqq1960180796@163.com>
2025-09-12 18:13:37 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,17 +15,6 @@ if status is-interactive
     alias idf-setup='source /opt/esp-idf/export.sh'
     alias h='hyprland'
     alias c='cargo'
-    alias n='nmcli'
 
-    function rmcs-nvim
-        cd ~/workspace/RMCS/
-        docker-compose start rmcs-develop
-        nohup /bin/bash -c 'docker exec -u developer -d rmcs-develop /opt/nvim-linux-x86_64/bin/nvim --headless --listen localhost:6666 && neovide --server=localhost:6666' >/dev/null 2>&1 &
-    end
-
-    function rmcs-shell
-        cd ~/workspace/RMCS/
-        docker-compose start rmcs-develop
-        docker-compose exec -it rmcs-develop zsh
-    end
+    fish_add_path ~/.local/bin/
 end
