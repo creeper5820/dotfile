b47e1e0f20289a10f37e56b69ea0e361ab2c76c9
creeper5820 <cqq1960180796@163.com>
2025-11-05 19:31:06 +0800

    Update fish tools

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,16 +5,23 @@ if status is-interactive
     export PATH="$PATH:$HOME/.cargo/bin"
     export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
 
+    export XDG_CURRENT_DESKTOP=Hyprland
+    export XDG_SESSION_DESKTOP=Hyprland
+
+    export W820NB=0C:AE:BD:1A:B6:F9
+
     # Kitty
     [ xterm-kitty = xterm-kitty ] && alias ssh="kitty +kitten ssh"
 
     # Proxy
     alias with-proxy='env {http,https,socks5}_proxy=http://127.0.0.1:7890'
 
-    # Esp32 IDF
-    alias idf-setup='source /opt/esp-idf/export.sh'
     alias h='hyprland'
     alias c='cargo'
+    alias suvi='sudo -E nvim'
+    alias bt='bluetoothctl'
+    alias up='nmcli connect up'
+    alias down='nmcli connect down'
 
     fish_add_path ~/.local/bin/
 end
--- .config/fish/functions/rmcs.fish
+++ .config/fish/functions/rmcs.fish
@@ -32,27 +32,38 @@ function __rmcs_nvim
     echo "  启动容器 $container_name..."
     docker start $container_name
 
-    echo "  启动 nvim headless 服务..."
-    docker exec -u $developer_name -d $container_name $nvim_path --headless --listen $nvim_host:$nvim_port
-
-    # 等待端口打开
     set timeout 10
     set success 0
+    set port $nvim_port
+
+    echo "  检查可用端口并启动 nvim headless 服务..."
+
+    while true
+        if nc -z $nvim_host $port
+            echo "  端口 $port 已被占用，尝试下一个..."
+            set port (math $port + 1)
+        else
+            echo "  使用端口 $port 启动 nvim 服务..."
+            docker exec -u $developer_name -d $container_name $nvim_path --headless --listen $nvim_host:$port
+            break
+        end
+    end
+
     for i in (seq 1 $timeout)
-        if nc -z $nvim_host $nvim_port
-            echo "󰄵  nvim 服务已启动"
+        if nc -z $nvim_host $port
+            echo "  nvim 服务已启动在端口 $port"
             set success 1
             break
         else
-            echo "󰔟  等待 nvim 服务启动中... ($i/$timeout)"
+            echo "  等待 nvim 服务启动中... ($i/$timeout)"
             sleep 1
         end
     end
 
     if test $success -eq 1
         echo "  启动 neovide..."
-        nohup neovide --server=$nvim_host:$nvim_port >/dev/null 2>&1 &
+        nohup neovide --server=$nvim_host:$port >/dev/null 2>&1 &
     else
-        echo "󰅙  nvim 服务未能在 $timeout 秒内启动，neovide 未启动"
+        echo "  nvim 服务未能在 $timeout 秒内启动，neovide 未启动"
     end
 end
