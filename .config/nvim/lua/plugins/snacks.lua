ad40d25228b4e410d91205038ce2683f826f3197
creeper5820 <cqq1960180796@163.com>
2026-03-06 00:30:38 +0800

    Modify lazyvim floating terminal and add hyprland scrolling layout config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,14 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
     terminal = {
       win = {
         position = "float",
